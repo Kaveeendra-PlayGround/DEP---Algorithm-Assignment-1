f05a9930c1693fa0d0c6bd876a913c38c6a79b83
Kaveendra Randeera <khkrandeera@gmail.com>
2023-08-14 09:30:34 +0530

    Done Algo4.java

--- Algo4.java
+++ Algo4.java
@@ -3,11 +3,26 @@ public class Algo4 {
         
         int [ ] nums = {10,20,30,40,50};
 
-        System.out.print("{");
-        for (int i = 0; i < nums.length; i++) System.out.printf("%d,",nums[nums.length-1-i]);    
+        //System.out.print("{");
+        //for (int i = 0; i < nums.length; i++) System.out.printf("%d,",nums[nums.length-1-i]);    
       
-        System.out.print("\b");
-        System.out.println("}");
+        //System.out.print("\b");
+        //System.out.println("}");
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums.length-1-i < i) break;
+            int x = nums [i];
+            nums [i] = nums [nums.length-1-i];
+            nums [nums.length-1-i] = x;            
+        }
+        System.out.print("{");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.printf("%d,",nums[i]);            
+        }
+        System.out.print("\b}");
+
+
+
     }
     
 }
